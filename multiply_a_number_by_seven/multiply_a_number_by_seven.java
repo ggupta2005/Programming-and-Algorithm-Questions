1204de13c88ea376d20fb200425ffa2e5e6ee0cb
ggupta2005 <ggupta2005@gmail.com>
2016-07-19 23:18:41 -0700

    new: dev: C-code and java-code for multiplying a number by seven using bitwise operators
    Signed-off-by: ggupta2005 <ggupta2005@gmail.com>

---
+++ multiply_a_number_by_seven/multiply_a_number_by_seven.java
@@ -0,0 +1,52 @@
+/*
+ * This program multiplies a number by seven using bitwise operators.
+ * Bitwise operators are more efficient to use than the airthmetic
+ * operators. More on this can be found on the website:-
+ * http://www.geeksforgeeks.org/efficient-way-to-multiply-with-7/
+ */
+import java.io.*;
+import java.util.*;
+
+public class multiply_a_number_by_seven
+{
+    /*
+     * This function multiplies a number by seven using the following
+     * operations:-
+     * 1. By right-shifting the number by 3 bits we multiply the number
+     *    by 8.
+     * 2. If we now subtract the num from the right shifted number, then
+     *    we will get the result.
+     */
+    public static long multiply_by_seven (long num)
+    {
+        long eight_multiply = num << 3;
+
+        long seven_multiply = eight_multiply - num;
+
+        return(seven_multiply);
+    }
+
+    public static void main (String[] args)
+    {
+        /*
+         * Test 0: Multiply 0 by 7
+         */
+        assert 0 == multiply_by_seven(0);
+
+        /*
+         * Test 1: Multiply 1 by 7
+         */
+        assert 7 == multiply_by_seven(1);
+
+        /*
+         * Test 2: Multiply 10 by 7
+         */
+        assert 70 == multiply_by_seven(10);
+
+        /*
+         * Test 3: Multiply 49 by 7
+         */
+        assert 343 == multiply_by_seven(49);
+
+    }
+}
