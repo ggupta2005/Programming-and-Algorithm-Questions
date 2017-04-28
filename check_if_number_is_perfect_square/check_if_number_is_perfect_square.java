087c8a00320fccdaa1155e6b036a35df673866c5
ggupta2005 <ggupta2005@gmail.com>
2017-04-27 22:05:37 -0700

    new: dev: C-code and java-code for checking if a number is perfect square
    Signed-off-by: ggupta2005 <ggupta2005@gmail.com>

---
+++ check_if_number_is_perfect_square/check_if_number_is_perfect_square.java
@@ -0,0 +1,111 @@
+/*
+ * This program finds if a positive number is a perfect square.
+ */
+import java.io.*;
+import java.util.*;
+
+public class check_if_number_is_perfect_square
+{
+    /*
+     * This function returns 'true' if a number is a perfect square
+     * and 'false' otherwise. This function uses binary search to
+     * check if a number is a perfect square with 'long' data type
+     * to make sure that the computed square doesn't fall out of
+     * range for 'long'. The time complexity of this function is
+     * O(log(n)). The space complexity of this function is O(1).
+     */
+    public static boolean if_number_is_perfect_square (int num)
+    {
+        long low, high, mid, num_copy;
+
+        /*
+         * If the number is less than zero, then return 'false'
+         */
+        if (num < 0) {
+            return(false);
+        }
+
+        /*
+         * If the number is equal to zero, then return 'true'
+         */
+        if (num == 0) {
+            return(true);
+        }
+
+        low = 1; // Set 'low' to 1
+        high = num; // Set 'high' to number
+        num_copy = num;
+
+        /*
+         * Continue until 'low' is not greater than 'high'
+         */
+        while (low <= high) {
+
+            /*
+             * Compute the 'mid' using the average of 'low'
+             * and 'high'
+             */
+            mid = low + ((high - low) / 2);
+
+            if ((mid * mid) == num_copy) {
+
+                /*
+                 * If square of 'mid' is same as number, then
+                 * return 'true'
+                 */
+                return(true);
+            } else if ((mid * mid) < num_copy) {
+
+                /*
+                 * If square of 'mid' is less than the number,
+                 * then set 'low' to one more than 'mid'
+                 */
+                low = mid + 1;
+            } else {
+
+                /*
+                 * If square of 'mid' is greater than the number,
+                 * then set 'high' to one less than 'mid'
+                 */
+                high = mid - 1;
+            }
+        }
+
+        /*
+         * If the number is not a perfect square, then return
+         * 'false'
+         */
+        return(false);
+    }
+
+    public static void main (String[] args)
+    {
+        /*
+         * Test 0: Test with negative numbers. They should not be
+         *         perfect squares.
+         */
+        assert(false == if_number_is_perfect_square(-1));
+        assert(false == if_number_is_perfect_square(Integer.MIN_VALUE));
+
+        /*
+         * Test 1: Test with positive numbers which are perfect
+         *         squares. They should be perfect squares.
+         */
+        assert(true == if_number_is_perfect_square(0));
+        assert(true == if_number_is_perfect_square(1));
+        assert(true == if_number_is_perfect_square(4));
+        assert(true == if_number_is_perfect_square(11 * 11));
+        assert(true == if_number_is_perfect_square(600 * 600));
+        assert(true ==
+                if_number_is_perfect_square((1 << 15) *
+                                                  (1 << 15)));
+
+        /*
+         * Test 2: Test with positive numbers which are not perfect
+         *         squares. They should not be perfect squares.
+         */
+        assert(false == if_number_is_perfect_square(2));
+        assert(false == if_number_is_perfect_square(Integer.MAX_VALUE));
+        assert(false == if_number_is_perfect_square(401));
+    }
+}
