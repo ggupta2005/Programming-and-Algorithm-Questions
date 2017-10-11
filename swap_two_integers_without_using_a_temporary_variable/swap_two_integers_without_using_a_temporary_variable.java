6f4b521389c75a6b286205a5c72d2424be60f413
ggupta2005 <ggupta2005@gmail.com>
2017-10-10 22:33:35 -0700

    new: dev: java-code for swapping the values in two variables without using a temporary variable
    Signed-off-by: ggupta2005 <ggupta2005@gmail.com>

---
+++ swap_two_integers_without_using_a_temporary_variable/swap_two_integers_without_using_a_temporary_variable.java
@@ -0,0 +1,24 @@
+/*
+ * This program swaps two the values in two variables without using
+ * a temporary variable.
+ */
+import java.io.*;
+import java.util.*;
+
+public class swap_two_integers_without_using_a_temporary_variable
+{
+    /*
+     * Not with primitive types (int, long, char, etc). Java passes
+     * stuff by value, which means the variable your function gets
+     * passed is a copy of the original, and any changes you make to
+     * the copy won't affect the original.
+     *
+     * Now, objects are a bit different, in that the "value" of an
+     * object variable is actually a reference to an object and
+     * copying the reference makes it point at the exact same object.
+     */
+    public static void main (String[] args)
+    {
+
+    }
+}
